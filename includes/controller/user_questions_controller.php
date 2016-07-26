3f5c269fab0f644e3c0570fc43bd4c07fd675422
kamishetty sreeja <sreeja25kamishetty@gmail.com>
2016-07-26 17:12:26 +0530

    User questions

--- includes/controller/user_questions_controller.php
+++ includes/controller/user_questions_controller.php
@@ -2,28 +2,24 @@
 function questions_title() {
   return _("Ask an archangel");
 }
-
 function user_questions() {
   global $user;
-
   if (! isset($_REQUEST['action'])) {
-    $open_questions = sql_select("SELECT * FROM `Questions` WHERE `AID` IS NULL AND `UID`='" . sql_escape($user['UID']) . "'");
-
-    $answered_questions = sql_select("SELECT * FROM `Questions` WHERE NOT `AID` IS NULL AND `UID`='" . sql_escape($user['UID']) . "'");
+    $open_questions = select_open_questions($user['UID']);
+    $answered_questions = select_ansd_questions($user['UID']);
     foreach ($answered_questions as &$question) {
       $answer_user_source = User($question['AID']);
       if ($answer_user_source === false)
         engelsystem_error(_("Unable to load user."));
       $question['answer_user'] = User_Nick_render($answer_user_source);
     }
-
     return Questions_view($open_questions, $answered_questions, page_link_to("user_questions") . '&action=ask');
   } else {
     switch ($_REQUEST['action']) {
       case 'ask':
         $question = strip_request_item_nl('question');
         if ($question != "") {
-          $result = sql_query("INSERT INTO `Questions` SET `UID`='" . sql_escape($user['UID']) . "', `Question`='" . sql_escape($question) . "'");
+          $result = insert_new_question($user['UID'], $question);
           if ($result === false)
             engelsystem_error(_("Unable to save question."));
           success(_("You question was saved."));
@@ -38,10 +34,9 @@ function user_questions() {
           $id = $_REQUEST['id'];
         else
           return error(_("Incomplete call, missing Question ID."), true);
-
-        $question = sql_select("SELECT * FROM `Questions` WHERE `QID`='" . sql_escape($id) . "' LIMIT 1");
+        $question = select_ques_by_id($id);
         if (count($question) > 0 && $question[0]['UID'] == $user['UID']) {
-          sql_query("DELETE FROM `Questions` WHERE `QID`='" . sql_escape($id) . "' LIMIT 1");
+          delete_ques_by_id($id);
           redirect(page_link_to("user_questions"));
         } else
           return page_with_title(questions_title(), array(
