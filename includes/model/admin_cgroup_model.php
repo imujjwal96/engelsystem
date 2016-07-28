d1c6bca9ec4fab14203725cef03698f4a90158dd
Dishant Khanna <dishant.khanna1807@gmail.com>
2016-07-29 00:16:43 +0530

    Update: Added group privileges query

--- includes/model/admin_cgroup_model.php
+++ includes/model/admin_cgroup_model.php
@@ -7,4 +7,8 @@ function create_new_group($name, $uid) {
     `UID`='" . sql_escape($uid) . "'");
 }
 
+function inserts_into_group_privileges($uid, $priv) {
+ return sql_query("INSERT INTO `GroupPrivileges` SET `group_id`='" . sql_escape($uid) . "', `privilege_id`='" . sql_escape($priv) . "'");
+}
+
 ?>
